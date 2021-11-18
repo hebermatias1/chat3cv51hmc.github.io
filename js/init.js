af039f9999a8bdb2475c6dce3aee46fc01b3e39a
Matias Cartagena Heber <83931183+hebermatias1@users.noreply.github.com>
2021-11-17 20:42:00 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDVQsKLGleKZYwPSvX37ZBE257657eXqTo",
+    authDomain: "fbautentifi.firebaseapp.com",
+    projectId: "fbautentifi",
+    storageBucket: "fbautentifi.appspot.com",
+    messagingSenderId: "1038299267725",
+    appId: "1:1038299267725:web:7e18438b0c0daf74de18c4",
+    measurementId: "G-TR6H62HQXP"
+    });
